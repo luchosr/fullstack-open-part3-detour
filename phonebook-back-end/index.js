03af30498a48f1cf3fc3ab7e8bfcb71137329760
Luciano Ramello <luchosr@gmail.com>
2024-05-22 00:31:41 +0200

    last updates

--- phonebook-back-end/index.js
+++ phonebook-back-end/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
 const app = express()
 
 app.use(express.static('dist'))
@@ -12,28 +14,57 @@ app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-]
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+
+// const url = `mongodb+srv://luchosr:${password}@fsopencluster0.xyz4huo.mongodb.net/ponebookApp?retryWrites=true&w=majority&appName=FSOpenCluster0`
+
+mongoose.set('strictQuery', false)
+// mongoose.connect(url)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+// const personSchema = new mongoose.Schema({
+//   name: String,
+//   number: String,
+// })
+
+// const Person = mongoose.model('Person', personSchema)
+
+// let persons = [
+//   {
+//     id: 1,
+//     name: 'Arto Hellas',
+//     number: '040-123456',
+//   },
+//   {
+//     id: 2,
+//     name: 'Ada Lovelace',
+//     number: '39-44-5323523',
+//   },
+//   {
+//     id: 3,
+//     name: 'Dan Abramov',
+//     number: '12-43-234345',
+//   },
+//   {
+//     id: 4,
+//     name: 'Mary Poppendieck',
+//     number: '39-23-6423122',
+//   },
+// ]
 
 const date = new Date()
 
@@ -44,20 +75,28 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then((persons) => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find((person) => person.id === id)
-
-  if (person) {
-    console.log('Person es: ', person)
+  Person.findById(request.params.id).then((person) => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
+
+// app.get('/api/persons/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   const person = persons.find((person) => person.id === id)
+
+//   if (person) {
+//     console.log('Person es: ', person)
+//     response.json(person)
+//   } else {
+//     response.status(404).end()
+//   }
+// })
 
 const generateId = () => {
   const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0
@@ -67,37 +106,53 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.name) {
-    return response.status(400).json({
-      error: 'content missing',
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
-
   const person = {
     name: body.name,
     number: body.number,
     id: generateId(),
   }
-
-  if (
-    persons.some(
-      (person) =>
-        person.name.toLocaleLowerCase() == body.name.toLocaleLowerCase()
-    )
-  ) {
-    return response.status(400).json({
-      error: 'name must be unique',
-    })
-  } else if (persons.some((person) => person.number == body.number)) {
-    return response.status(400).json({
-      error: 'number must be unique',
-    })
-  } else {
-    persons = persons.concat(person)
-
-    response.json(person)
-  }
+  person.save().then((savedPerson) => {
+    response.json(savedPerson)
+  })
 })
+
+// app.post('/api/persons', (request, response) => {
+//   const body = request.body
+
+//   if (!body.name) {
+//     return response.status(400).json({
+//       error: 'content missing',
+//     })
+//   }
+
+//   const person = {
+//     name: body.name,
+//     number: body.number,
+//     id: generateId(),
+//   }
+
+//   if (
+//     persons.some(
+//       (person) =>
+//         person.name.toLocaleLowerCase() == body.name.toLocaleLowerCase()
+//     )
+//   ) {
+//     return response.status(400).json({
+//       error: 'name must be unique',
+//     })
+//   } else if (persons.some((person) => person.number == body.number)) {
+//     return response.status(400).json({
+//       error: 'number must be unique',
+//     })
+//   } else {
+//     persons = persons.concat(person)
+
+//     response.json(person)
+//   }
+// })
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
@@ -106,7 +161,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
