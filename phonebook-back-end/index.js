8ba1bdbdf9acaeca2cc76ef4ee9ce444f665879f
Luciano Ramello <luchosr@gmail.com>
2024-05-24 10:21:31 +0200

    update 3.15

--- phonebook-back-end/index.js
+++ phonebook-back-end/index.js
@@ -1,167 +1,112 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const Person = require('./models/person')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const Person = require("./models/person");
+const app = express();
 
-app.use(express.static('dist'))
-const cors = require('cors')
-app.use(express.json())
-app.use(cors())
-app.use(morgan('tiny'))
-morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(express.static("dist"));
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+app.use(morgan("tiny"));
+morgan.token("body", (req) => JSON.stringify(req.body));
 app.use(
-  morgan(':method :url :status :res[content-length] - :response-time ms :body')
-)
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-// const url = `mongodb+srv://luchosr:${password}@fsopencluster0.xyz4huo.mongodb.net/ponebookApp?retryWrites=true&w=majority&appName=FSOpenCluster0`
+mongoose.set("strictQuery", false);
 
-mongoose.set('strictQuery', false)
-// mongoose.connect(url)
+const url = process.env.MONGODB_URI;
 
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
+console.log("connecting to", url);
 
 mongoose
   .connect(url)
   .then((result) => {
-    console.log('connected to MongoDB')
+    console.log("connected to MongoDB");
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
-// const personSchema = new mongoose.Schema({
-//   name: String,
-//   number: String,
-// })
+const date = new Date();
 
-// const Person = mongoose.model('Person', personSchema)
-
-// let persons = [
-//   {
-//     id: 1,
-//     name: 'Arto Hellas',
-//     number: '040-123456',
-//   },
-//   {
-//     id: 2,
-//     name: 'Ada Lovelace',
-//     number: '39-44-5323523',
-//   },
-//   {
-//     id: 3,
-//     name: 'Dan Abramov',
-//     number: '12-43-234345',
-//   },
-//   {
-//     id: 4,
-//     name: 'Mary Poppendieck',
-//     number: '39-23-6423122',
-//   },
-// ]
-
-const date = new Date()
-
-app.get('/info', (request, response) => {
+app.get("/info", (request, response) => {
   response.send(
     `<p>Phonebook has info for ${persons.length} people</p><br/><p>${date}</p>`
-  )
-})
+  );
+});
 
-app.get('/api/persons', (request, response) => {
+app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => {
-    response.json(persons)
-  })
-})
+    response.json(persons);
+  });
+});
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person)
-  })
-})
-
-// app.get('/api/persons/:id', (request, response) => {
-//   const id = Number(request.params.id)
-//   const person = persons.find((person) => person.id === id)
-
-//   if (person) {
-//     console.log('Person es: ', person)
-//     response.json(person)
-//   } else {
-//     response.status(404).end()
-//   }
-// })
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
+});
 
 const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0
-  return Math.round(Math.random() * maxId * 2) + 1
-}
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return Math.round(Math.random() * maxId * 2) + 1;
+};
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
 
   if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
+    return response.status(400).json({ error: "content missing" });
   }
   const person = {
     name: body.name,
     number: body.number,
     id: generateId(),
-  }
+  };
   person.save().then((savedPerson) => {
-    response.json(savedPerson)
+    response.json(savedPerson);
+  });
+});
+
+app.delete('/api/persons/:id', (request, response, next) => {
+
+  Person.findByIdAndDelete(request.params.id)
+  .then(result => {
+    response.status(204).end()
   })
+  .catch(error => next(error))
+});
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person , { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
-// app.post('/api/persons', (request, response) => {
-//   const body = request.body
 
-//   if (!body.name) {
-//     return response.status(400).json({
-//       error: 'content missing',
-//     })
-//   }
 
-//   const person = {
-//     name: body.name,
-//     number: body.number,
-//     id: generateId(),
-//   }
-
-//   if (
-//     persons.some(
-//       (person) =>
-//         person.name.toLocaleLowerCase() == body.name.toLocaleLowerCase()
-//     )
-//   ) {
-//     return response.status(400).json({
-//       error: 'name must be unique',
-//     })
-//   } else if (persons.some((person) => person.number == body.number)) {
-//     return response.status(400).json({
-//       error: 'number must be unique',
-//     })
-//   } else {
-//     persons = persons.concat(person)
-
-//     response.json(person)
-//   }
-// })
-
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter((person) => person.id !== id)
-
-  response.status(204).end()
-})
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
