0a38fc28418fd0429c55ecb728eb0f614c1f9089
Luciano Ramello <luchosr@gmail.com>
2024-05-24 10:30:21 +0200

    update 3.16

--- phonebook-back-end/index.js
+++ phonebook-back-end/index.js
@@ -105,6 +105,19 @@ app.put('/api/persons/:id', (request, response, next) => {
 })
 
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+app.use(unknownEndpoint);
+
+const errorHandler = (error,request,response,next)=>{
+console.error(error.message);
+if(error.name === 'CastError'){
+  return response.status(400).send({error:'malformatted id'});
+}
+next(error);
+}
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
