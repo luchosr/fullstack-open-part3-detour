eeae98ec29d88c611f0cef1eae174be99020a052
Luciano Ramello <luchosr@gmail.com>
2024-05-24 14:46:59 +0200

    update linter

--- phonebook-back-end/mongo.js
+++ phonebook-back-end/mongo.js
@@ -1,43 +1,51 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit(1)
+  console.log('give password as argument');
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url = `mongodb+srv://luchosr:${password}@fsopencluster0.xyz4huo.mongodb.net/ponebookApp?retryWrites=true&w=majority&appName=FSOpenCluster0`
+const url = `mongodb+srv://luchosr:${password}@fsopencluster0.xyz4huo.mongodb.net/ponebookApp?retryWrites=true&w=majority&appName=FSOpenCluster0`;
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 
-mongoose.connect(url)
+mongoose.connect(url);
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
+});
 
-const Person = mongoose.model('Person', personSchema)
+const Person = mongoose.model('Person', personSchema);
 
 if (process.argv[3]) {
   const person = new Person({
     name: process.argv[3],
     number: process.argv[4],
-  })
+  });
 
   person.save().then((result) => {
     console.log(
       `added ${process.argv[3]} number ${process.argv[4]} to phonebook`
-    )
-    mongoose.connection.close()
-  })
+    );
+    mongoose.connection.close();
+  });
 } else {
   Person.find({}).then((result) => {
-    console.log('Phonebook')
+    console.log('Phonebook');
     result.forEach((person) => {
-      console.log(person.name + ' ' + person.number)
-    })
-    mongoose.connection.close()
-  })
+      console.log(person.name + ' ' + person.number);
+    });
+    mongoose.connection.close();
+  });
 }
