f6db12dab52c87bd5d1e21def7a2b002fece45a3
Luciano Ramello <luchosr@gmail.com>
2024-05-27 17:15:10 +0200

    update post action

--- blogList/index.js
+++ blogList/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
-const blogs = [
+let blogs = [
   {
     title: 'My first blog',
     author: 'Luciano',
     url: 'www.global.com',
     likes: 2,
+    id: 1,
   },
   {
     title: 'My second blog',
     author: 'Alberto',
     url: 'www.globalactions.com',
     likes: 6,
+    id: 2,
   },
 ];
 // const cors = require('cors');
@@ -31,7 +34,6 @@ const blogs = [
 // mongoose.connect(mongoUrl);
 
 // app.use(cors());
-// app.use(express.json());
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
@@ -41,6 +43,48 @@ app.get('/api/blogs', (request, response) => {
   response.json(blogs);
 });
 
+app.get('/api/blogs/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const blog = blogs.find((blog) => blog.id === id);
+  if (blog) {
+    response.json(blog);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete('/api/blogs/:id', (request, response) => {
+  const id = Number(request.params.id);
+  blogs = blogs.filter((blog) => blog.id !== id);
+
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId = blogs.length > 0 ? Math.max(...blogs.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post('/api/blogs', (request, response) => {
+  const body = request.body;
+  if (!body.title) {
+    return response.status(400).json({
+      error: 'title missing',
+    });
+  }
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    id: generateId(),
+  };
+
+  blogs = blogs.concat(blog);
+
+  response.json(blog);
+});
 // app.get('/api/blogs', (request, response) => {
 //   Blog.find({}).then((blogs) => {
 //     response.json(blogs);
