1aaaf98be89da1cdf652b855e90832d85fbfdef0
Luciano Ramello <luchosr@gmail.com>
2024-05-29 13:08:58 +0200

    update blog list index

--- blogList/index.js
+++ blogList/index.js
@@ -1,32 +1,48 @@
+require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
+const Blog = require('./models/blog');
+
 const app = express();
+
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+app.use(cors());
 app.use(express.json());
 
-let blogs = [
-  {
-    title: 'My first blog',
-    author: 'Luciano',
-    url: 'www.global.com',
-    likes: 2,
-    id: 1,
-  },
-  {
-    title: 'My second blog',
-    author: 'Alberto',
-    url: 'www.globalactions.com',
-    likes: 6,
-    id: 2,
-  },
-];
-// const cors = require('cors');
-// const mongoose = require('mongoose');
+app.use(morgan('tiny'));
+morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
 
-// const blogSchema = new mongoose.Schema({
-//   title: String,
-//   author: String,
-//   url: String,
-//   likes: Number,
-// });
+// let blogs = [
+//   {
+//     title: 'My first blog',
+//     author: 'Luciano',
+//     url: 'www.global.com',
+//     likes: 2,
+//     id: 1,
+//   },
+//   {
+//     title: 'My second blog',
+//     author: 'Alberto',
+//     url: 'www.globalactions.com',
+//     likes: 6,
+//     id: 2,
+//   },
+// ];
+
+const mongoose = require('mongoose');
+
+const password = process.argv[2];
+
+mongoose.set('strictQuery', false);
+
+const url = process.env.MONGODB_URI;
+
+console.log('connecting to', url);
 
 // const Blog = mongoose.model('Blog', blogSchema);
 
@@ -35,29 +51,124 @@ let blogs = [
 
 // app.use(cors());
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
+// const password = process.argv[2];
+
+// const url = `mongodb+srv://luchosr:${password}@fsopencluster0.xyz4huo.mongodb.net/Bloglist?retryWrites=true&w=majority&appName=FSOpenCluster0`;
+
+// mongoose.set('strictQuery', false);
+// mongoose.connect(url);
+
+// const blogSchema = new mongoose.Schema({
+//   title: String,
+//   author: String,
+//   url: String,
+//   likes: Number,
+// });
+
+// blogSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString();
+//     delete returnedObject._id;
+//     delete returnedObject.__v;
+//   },
+// });
+
+// const Blog = mongoose.model('Blog', blogSchema);
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+const date = new Date();
+
+app.get('/info', (request, response) => {
+  Blog.estimatedDocumentCount({}).then((count) =>
+    response.send(
+      `<p>Bloglist has info for ${count} blogs</p><br/><p>${date}</p>`
+    )
+  );
 });
 
-app.get('/api/blogs', (request, response) => {
-  response.json(blogs);
+app.get('/api/blogs', (request, response, next) => {
+  Blog.find({})
+    .then((blog) => {
+      response.json(blogs);
+    })
+    .catch((error) => next(error));
 });
 
-app.get('/api/blogs/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const blog = blogs.find((blog) => blog.id === id);
-  if (blog) {
-    response.json(blog);
-  } else {
-    response.status(404).end();
+app.get('/api/blogs/:id', (request, response, next) => {
+  // const id = Number(request.params.id);
+  // const blog = blogs.find((blog) => blog.id === id);
+  // if (blog) {
+  //   response.json(blog);
+  // } else {
+  //   response.status(404).end();
+  // }
+
+  Blog.findById(request.params.id)
+    .then((blog) => {
+      if (blog) {
+        response.json(blog);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+app.post('/api/blogs', (request, response) => {
+  // const body = request.body;
+  // if (!body.title) {
+  //   return response.status(400).json({
+  //     error: 'title missing',
+  //   });
+  // }
+
+  // const blog = {
+  //   title: body.title,
+  //   author: body.author,
+  //   url: body.url,
+  //   likes: body.likes,
+  //   id: generateId(),
+  // };
+
+  // blogs = blogs.concat(blog);
+
+  // response.json(blog);
+  const { title, author, url, likes } = request.body;
+
+  if (!title) {
+    return response.status(400).json({ error: 'title missing' });
+  } else if (!author) {
+    return response.status(400).json({ error: 'author missing' });
   }
+
+  const blog = new Blog({ title, author, url, likes });
+  blog
+    .save()
+    .then((savedBlog) => {
+      response.json(savedBlog);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete('/api/blogs/:id', (request, response) => {
-  const id = Number(request.params.id);
-  blogs = blogs.filter((blog) => blog.id !== id);
+app.delete('/api/blogs/:id', (request, response, next) => {
+  // const id = Number(request.params.id);
+  // blogs = blogs.filter((blog) => blog.id !== id);
 
-  response.status(204).end();
+  // response.status(204).end();
+
+  Blog.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 const generateId = () => {
@@ -65,26 +176,6 @@ const generateId = () => {
   return maxId + 1;
 };
 
-app.post('/api/blogs', (request, response) => {
-  const body = request.body;
-  if (!body.title) {
-    return response.status(400).json({
-      error: 'title missing',
-    });
-  }
-
-  const blog = {
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-    id: generateId(),
-  };
-
-  blogs = blogs.concat(blog);
-
-  response.json(blog);
-});
 // app.get('/api/blogs', (request, response) => {
 //   Blog.find({}).then((blogs) => {
 //     response.json(blogs);
@@ -99,7 +190,41 @@ app.post('/api/blogs', (request, response) => {
 //   });
 // });
 
-const PORT = 3003;
+app.put('/api/blogs/:id', (request, response, next) => {
+  const { title, author, url, likes } = request.body;
+
+  const blog = {
+    title,
+    author,
+    url,
+    likes,
+  };
+  if (!title || !author) {
+    return response.status(400).json({ error: 'title or author missing' });
+  }
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then((updatedBlog) => {
+      response.json(updatedBlog);
+    })
+    .catch((error) => next(error));
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+  next(error);
+};
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ blogList/mongo.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('give password as argument');
+  process.exit(1);
+}
+
+// const password = process.argv[2];
+
+// const url = `mongodb+srv://luchosr:${password}@fsopencluster0.xyz4huo.mongodb.net/Bloglist?retryWrites=true&w=majority&appName=FSOpenCluster0`;
+
+// mongoose.set('strictQuery', false);
+
+// mongoose.connect(url);
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+  important: Boolean,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+const blog = new Blog({
+  title: 'My mongoose blog',
+  author: 'Mongo',
+  url: 'www.globalactions.com',
+  likes: 6,
+});
+
+// blog.save().then((result) => {
+//   console.log('blog saved!');
+//   mongoose.connection.close();
+// });
+
+if (process.argv[3]) {
+  const blog = new Blog({
+    title: process.argv[3],
+    author: process.argv[4],
+    url: process.argv[5],
+    likes: process.argv[6],
+  });
+
+  blog.save().then((result) => {
+    console.log(
+      `added ${process.argv[3]} blog from ${process.argv[4]} to Bloglist`
+    );
+    mongoose.connection.close();
+  });
+} else {
+  Blog.find({}).then((result) => {
+    console.log('Bloglist');
+    result.forEach((blog) => {
+      console.log(blog.title + ' by ' + blog.author + ', url: ' + blog.url);
+    });
+    mongoose.connection.close();
+  });
+}
